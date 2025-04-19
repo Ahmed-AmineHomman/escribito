aa1a561cd85e58d97a5f96dc9b6da9bfcfc0db10
ahmed-amine <ahmed-amine@TARIQ>
2025-04-19 18:17:00 +0200

    fixing a bug causing error when conversation was empty

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ from functools import partial
 from os import getenv
 
 import gradio as gr
-from PIL.ImageOps import scale
 from cohere import ClientV2
 
-from app_api import next_message, download_conversation, CHARACTERS, MODELS
+from app_api import next_message, download_conversation, MODELS
 
 
 def load_parameters() -> Namespace:
@@ -50,6 +49,18 @@ def load_parameters() -> Namespace:
     return parser.parse_args()
 
 
+def update_role_selector(name_a, name_b, which_one: str):
+    """
+    Update the role selector based on the character names.
+    """
+    if which_one == "a":
+        return gr.Dropdown(choices=[name_a, name_b], value=name_a)
+    elif which_one == "b":
+        return gr.Dropdown(choices=[name_a, name_b], value=name_b)
+    else:
+        raise Exception("Unknown role selector.")
+
+
 def build_ui(
         config: dict,
         client: ClientV2,
@@ -77,96 +88,108 @@ def build_ui(
             with gr.Column(scale=3):
                 gr.Markdown(config.get('description', ''))
 
-        with gr.Row(equal_height=True):
-            # Left panel: conversation & inputs
-            with gr.Column(scale=5):
-                # The dialogue displayed as a chatbot
-                conversation = gr.Chatbot(
-                    label="Story",
-                    type="messages",
-                    container=True,
-                    editable="all",
-                    avatar_images=("config/avatars/character_a.png", "config/avatars/character_b.png"),
-                    height=600,
-                    show_copy_button=True,
-                    show_copy_all_button=True,
+        # Control Panels
+        with gr.Tab(label="Character A"):  # Character A control panel
+            gr.Markdown(
+                f"## {config.get('character_panel').get('title')}\n\n{config.get('character_panel').get('description')}"
+            )
+            name_a = gr.Text(
+                label=config.get("character_panel").get("character_a_name_label"),
+                value=config.get("character_panel").get("character_a_name")
+            )
+            story_a = gr.Textbox(
+                label=config.get("character_panel").get("character_a_story_label"),
+                value=config.get("character_panel").get("character_a_story"),
+                lines=3,
+            )
+        with gr.Tab(label="Character B"):  # Character B control panel
+            gr.Markdown(
+                f"## {config.get('character_panel').get('title')}\n\n{config.get('character_panel').get('description')}"
+            )
+            name_b = gr.Text(
+                label=config.get("character_panel").get("character_b_name_label"),
+                value=config.get("character_panel").get("character_b_name")
+            )
+            story_b = gr.Text(
+                label=config.get("character_panel").get("character_b_story_label"),
+                value=config.get("character_panel").get("character_b_story"),
+                lines=3,
+            )
+        with gr.Tab(label="Model"):  # LLM Control Panel
+            gr.Markdown(
+                f"## {config.get('llm_panel').get('title')}\n\n{config.get('llm_panel').get('description')}")
+            with gr.Row():
+                model = gr.Dropdown(
+                    label=config.get("llm_panel").get("model_btn_label"),
+                    info=config.get("llm_panel").get("model_btn_info"),
+                    value=list(MODELS.keys())[0],
+                    choices=MODELS.keys(),
+                    multiselect=False,
+                )
+                temperature = gr.Slider(
+                    label=config.get("llm_panel").get("temperature_btn_label"),
+                    info=config.get("llm_panel").get("temperature_btn_info"),
+                    value=0.8,
+                    minimum=0.0,
+                    maximum=2.0,
+                    step=0.05,
                 )
 
-                # Inputs
-                with gr.Row(equal_height=True):
-                    role_selector = gr.Dropdown(
-                        value=CHARACTERS.get("user"),
-                        choices=list(CHARACTERS.values()),
-                        multiselect=False,
-                        label=config.get("user_panel").get("role_selector_label"),
-                        scale=1,
-                        container=True
-                    )
-                    text_input = gr.Textbox(
-                        label=config.get("user_panel").get("message_label"),
-                        placeholder="Type your message here",
-                        container=True,
-                        scale=3,
-                    )
-                    with gr.Column():
-                        send_btn = gr.Button(
-                            value=config.get("user_panel").get("send_btn_label"),
-                            variant="primary"
-                        )
-                        download_btn = gr.DownloadButton(
-                            label=config.get("user_panel").get("download_btn_label"),
-                            variant="secondary"
-                        )
+        # Story Display
+        conversation = gr.Chatbot(
+            label="Story",
+            type="messages",
+            container=True,
+            editable="all",
+            avatar_images=("config/avatars/character_a.png", "config/avatars/character_b.png"),
+            height=600,
+            show_copy_button=True,
+            show_copy_all_button=True,
+        )
 
-            # Right Panel: controls
-            with gr.Column(scale=3):
-                # Character Definition Panel
-                gr.Markdown(
-                    f"## {config.get('character_panel').get('title')}\n\n{config.get('character_panel').get('description')}")
-                with gr.Column(scale=1, variant="panel"):
-                    with gr.Tab(label="Character A"):
-                        name_a = gr.Text(
-                            label=config.get("character_panel").get("character_a_name_label"),
-                            value=config.get("character_panel").get("character_a_name")
-                        )
-                        story_a = gr.Textbox(
-                            label=config.get("character_panel").get("character_a_story_label"),
-                            value=config.get("character_panel").get("character_a_story"),
-                            lines=3,
-                        )
-                    with gr.Tab(label="Character B"):
-                        name_b = gr.Text(
-                            label=config.get("character_panel").get("character_b_name_label"),
-                            value=config.get("character_panel").get("character_b_name")
-                        )
-                        story_b = gr.Text(
-                            label=config.get("character_panel").get("character_b_story_label"),
-                            value=config.get("character_panel").get("character_b_story"),
-                            lines=3,
-                        )
-
-                # LLM Control Panel
-                gr.Markdown(
-                    f"## {config.get('llm_panel').get('title')}\n\n{config.get('llm_panel').get('description')}")
-                with gr.Column(scale=1, variant="panel"):
-                    model = gr.Dropdown(
-                        label=config.get("llm_panel").get("model_btn_label"),
-                        info=config.get("llm_panel").get("model_btn_info"),
-                        value=list(MODELS.keys())[0],
-                        choices=MODELS.keys(),
-                        multiselect=False,
-                    )
-                    temperature = gr.Slider(
-                        label=config.get("llm_panel").get("temperature_btn_label"),
-                        info=config.get("llm_panel").get("temperature_btn_info"),
-                        value=0.8,
-                        minimum=0.0,
-                        maximum=2.0,
-                        step=0.05,
-                    )
+        # User Input Panel
+        with gr.Row(equal_height=True):
+            role_selector = gr.Dropdown(
+                value=config.get("character_panel").get("character_a_name"),
+                choices=[
+                    config.get("character_panel").get("character_a_name"),
+                    config.get("character_panel").get("character_b_name")
+                ],
+                multiselect=False,
+                label=config.get("user_panel").get("role_selector_label"),
+                scale=1,
+                container=True
+            )
+            text_input = gr.Textbox(
+                label=config.get("user_panel").get("message_label"),
+                placeholder="Type your message here",
+                container=True,
+                scale=3,
+            )
+            with gr.Column():
+                send_btn = gr.Button(
+                    value=config.get("user_panel").get("send_btn_label"),
+                    variant="primary"
+                )
+                download_btn = gr.DownloadButton(
+                    label=config.get("user_panel").get("download_btn_label"),
+                    variant="secondary"
+                )
 
         # States
         temp_dir = gr.State(value=temp_dir)
+
+        # Whenever the character name is changed, update the CHARACTERS dictionary.
+        name_a.change(
+            fn=partial(update_role_selector, which_one="a"),
+            inputs=[name_a, name_b],
+            outputs=role_selector
+        )
+        name_b.change(
+            fn=partial(update_role_selector, which_one="b"),
+            inputs=[name_a, name_b],
+            outputs=role_selector
+        )
 
         # When the button is clicked, call next_message and update both the conversation display and state.
         send_btn.click(
